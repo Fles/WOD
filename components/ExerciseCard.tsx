be120f79bac50a3d4151033dc1533c3457ce2055
Nikola Horvat <the.final.frontend@gmail.com>
2020-03-23 11:26:44 +0100

    feat: add exercises page and description to card

--- components/ExerciseCard.tsx
+++ components/ExerciseCard.tsx
@@ -6,9 +6,43 @@ import Avatar from '@material-ui/core/Avatar'
 import IconButton from '@material-ui/core/IconButton'
 import FavoriteIcon from '@material-ui/icons/Favorite'
 import LinearProgress from '@material-ui/core/LinearProgress'
+import Typography from '@material-ui/core/Typography'
+import Collapse from '@material-ui/core/Collapse'
+import CardContent from '@material-ui/core/CardContent'
+import CardActions from '@material-ui/core/CardActions'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+import clsx from 'clsx'
+import { makeStyles } from '@material-ui/core/styles'
 
-export default function ExerciseCard({ name, media, time, progress }) {
+const useStyles = makeStyles(theme => ({
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+}))
+export default function ExerciseCard({
+  name,
+  media,
+  time,
+  progress,
+  instructions = '',
+  target = [],
+  difficulty = '',
+}) {
+  const classes = useStyles()
+
   const normalise = value => ((value - 0) * 100) / (time - 0)
+  const [expanded, setExpanded] = React.useState(false)
+  const handleExpandClick = () => {
+    setExpanded(!expanded)
+  }
+
   return (
     <Card>
       <CardHeader
@@ -27,6 +61,26 @@ export default function ExerciseCard({ name, media, time, progress }) {
         autoPlay={true}
         loop={true}
       />
+      <CardActions disableSpacing>
+        <IconButton
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expanded,
+          })}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>Target muscles: {target.join(', ')}</Typography>
+          <Typography paragraph>Difficulty: {difficulty}</Typography>
+          <Typography paragraph>Instructions: {instructions}</Typography>
+        </CardContent>
+      </Collapse>
+
       {progress && (
         <LinearProgress
           variant="determinate"
---
+++ pages/exercises.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import { GetStaticProps, GetStaticPaths, GetServerSideProps } from 'next'
+import fetch from 'isomorphic-unfetch'
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid'
+import Paper from '@material-ui/core/Paper'
+
+import ExerciseCard from '../components/ExerciseCard'
+
+const Exercises = ({ exercises }) => {
+  return (
+    <Grid
+      container
+      spacing={2}
+      direction="column"
+      justify="center"
+      alignItems="center"
+    >
+      {exercises.map(e => (
+        <Grid container item xs={6} spacing={6}>
+          <Grid item>
+            <ExerciseCard {...e} />
+          </Grid>
+        </Grid>
+      ))}
+    </Grid>
+  )
+}
+
+Exercises.getInitialProps = async function() {
+  //const res = await fetch('https://api.')
+  //const data = await res.json()
+  const exe = await require('../public/data.json')
+
+  return {
+    exercises: exe.map(entry => entry),
+  }
+}
+
+export default Exercises
